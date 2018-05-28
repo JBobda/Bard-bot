be4639b4549871c3a89e6e61ae7d9412c6834420
JBobda <J.Bobda@g.fayar.net>
2018-05-27 23:34:51 -0500

    Streamlined the MessageListener class by splitting the code into multiple methods.

--- src/main/java/bot/janb/generaldiscordbot/MessageListener.java
+++ src/main/java/bot/janb/generaldiscordbot/MessageListener.java
@@ -7,6 +7,10 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class MessageListener extends ListenerAdapter{
+    
+    private Message message;
+    private MessageReceivedEvent event;
+    
     public void onMessageReceived(MessageReceivedEvent event){
         //Stops messages from other bot's to trigger commands.
         if(event.getAuthor().isBot()) return;
@@ -14,15 +18,23 @@ public class MessageListener extends ListenerAdapter{
         //Isolates comand prefix from the rest of the raw message.
         String rawMessage = event.getMessage().getContentRaw();
         String commandPrefix = getCommandPrefix(rawMessage);
-        rawMessage = rawMessage.substring(rawMessage.indexOf(" ") + 1);
-        String command = getCommand(rawMessage);
-        
-        
         //Checks if the command prefix has been included.
         if(!commandPrefix.equals("!apx")) return;
         
+        //Isolates command from the rest of the raw message.
+        rawMessage = rawMessage.substring(rawMessage.indexOf(" ") + 1);
+        String command = getCommand(rawMessage);
+        
+        //Calls the command menu.
+        this.event = event;
+        commandMenu(command, rawMessage);
+        
         MessageChannel channel = event.getChannel();
-        Message message;
+        
+        channel.sendMessage(message).queue();
+    }
+    
+    public void commandMenu(String command, String rawMessage){
         switch(command){
             case "mimic":
                 try{
@@ -65,20 +77,11 @@ public class MessageListener extends ListenerAdapter{
                         .build();
                 break;
             default:
-                try{
-                    rawMessage = rawMessage.substring(rawMessage.indexOf(" "));
-                    message = new MessageBuilder()
-                            .append(rawMessage)
-                            .build();
-                }catch(StringIndexOutOfBoundsException e){
-                    message = new MessageBuilder()
-                            .append("Please enter arguments after the mimic command.")
-                            .build();
-                }
+                message = new MessageBuilder()
+                        .append("\"" + command + "\" is not a valid command.")
+                        .build();
                 break;
         }
-        
-        channel.sendMessage(message).queue();
     }
     
     public String getCommandPrefix(String rawMessage){
